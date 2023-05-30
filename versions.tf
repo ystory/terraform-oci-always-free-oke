75f273ba15f381bf08a5af030703320d4515f00f
Leo Yongsul Kim <ystory84@gmail.com>
2023-05-31 08:07:14 +0900

    refactor: Remove unnecessary variables and add new ones (#2)
    - Remove unnecessary variables:
      - user_id
      - api_fingerprint
      - api_private_key
      - api_private_key_password
    
    - Add new variables:
      - control_plane_allowed_cidrs
      - control_plane_type

--- versions.tf
+++ versions.tf
@@ -1,8 +1,11 @@
 terraform {
+  required_version = ">= 1.2.0"
+
   required_providers {
     oci = {
-      source  = "oracle/oci"
-      version = "4.122.0"
+      source                = "oracle/oci"
+      configuration_aliases = [oci.home]
+      version               = "4.122.0"
     }
   }
 }
